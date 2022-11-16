bae093ff6b66fa964b0636a33bc7fc3cd17eab28
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-16 13:14:38 +0400

    setSearchResults(resultsArray)//устанавливаю результаты поиска равному массиву результата

--- src/api/SearchBar.js
+++ src/api/SearchBar.js
@@ -4,9 +4,20 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 import React from 'react'
 
 
-const SearchBar = ({post, setSearchResults}) => { //получает 
+const SearchBar = ({posts, setSearchResults}) => { //получает 
     const handleSubmit = (e) => e.preventDefault()//получает событие(е),e.preventDefault-по умолчанию
   
+    const handleSearchChange = (e) => { //обработка изменений
+        if (!e.target.value) return setSearchResults(posts) //если ввод пуст или стриаем всё что набирала вводе то вернуть набор результата поиска и установить (равным исхлдным сообщениям)
+
+       //массив результатов при поиске сообщений
+        const resultsArray = posts.filter(post => post.title.includes(e.target.value) || post.body.includes(e.target.value))//ус-ла сообщения.фильтрую эти сообщения для каждого =>заголовок сообщения метод строки и искать e.target.value и вернёт лог,значение если есть то что на входе поиска то true и будет включён в массив результата, || тоже самое с телом для каждого сообщения
+
+        setSearchResults(resultsArray)//устанавливаю результаты поиска равному массиву результата
+    }
+
+
+
     return (
   <header>
   <form  className="search" onSubmit={handleSubmit}>
@@ -16,8 +27,10 @@ const SearchBar = ({post, setSearchResults}) => { //получает
   id="search" //получает индификатор поиска
   onChange={handleSearchChange}//при изменении в другую функцию(обработка изменений)
 />
+<button className="search__button">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
   </form> {/*onSubmit={handleSumbit}-обработать отправку */}
-  
   </header>
   )
 }
